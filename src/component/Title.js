39832cb5b66f15b1fc7b6d18d4ba6d04761fe6bb
Michael Rockingham <mrockingham2@gmail.com>
2020-08-29 02:05:46 -0400

    updated title

--- src/component/Title.js
+++ src/component/Title.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Title = () => {
     return (
         <div className='title'>
-            <h1>FireGram</h1>
-            <h2>Your Pictures</h2>
+           
+            <h2>Michael Rockingham</h2>
             <p>This is random text that you don't need to worry about now</p>
         </div>
     )
